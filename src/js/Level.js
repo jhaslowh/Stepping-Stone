a6bc9a2c6767e29de059bbe29f8e92d6c400b87d
Jonathan Haslow-Hall <Jhaslowh@gmail.com>
2013-11-21 22:09:58 -0500

    Updated level.js
    Added more to init method.
    Fixed camera z value.

--- src/js/Level.js
+++ src/js/Level.js
@@ -3,9 +3,6 @@ function GameLevel(){
   // List of all blocks 
   this.blocks = [];
   
-  // Last correct path block placed 
-  this.last_path_block;
-  
   // Player
   this.player = new Player();
   
@@ -16,11 +13,15 @@ function GameLevel(){
   this.scene;
   
   // Generation variables 
+  // All these values will be set in init()
   this.next_gen_loc = 0; // Starting x value for next chunk of generated land 
   this.gen_min_y = 0;
   this.gen_max_y = 0;
   this.level_width = 0;
   this.level_height = 0;
+  
+  // Last correct path block placed 
+  this.last_path_block;
   
   // TODO remove later
   this.cube;
@@ -28,38 +29,48 @@ function GameLevel(){
 
 /** Initialize level */
 GameLevel.prototype.init = function (w,h){
-  // TODO Need to generate starting terrain (probably 3 screens worth) 
-  
-  // Set level width and height
-  this.level_width = w;
-  this.level_height = h;
-  
+  /** Set up rendering Code */
   // Move camera to middle of level width and height 
   this.camera_loc.x = w/2;
   this.camera_loc.y = h/2;
-  this.camera_loc.z = -200; // TODO need to fix this so one full screen fits in the camera 
+  /* Set the camera z based on the screen width, 
+   * so the whole level can be seen. */
+  this.camera_loc.z = (w/2)/Math.tan(22.5); 
   
   // Create Camera 
   this.camera = new THREE.PerspectiveCamera(
-      45,             // Field of view
+      45,       // Field of view
       w/h,      // Aspect ratio
-      0.1,            // Near plane
-      10000           // Far plane
+      0.1,      // Near plane
+      10000     // Far plane
   );
-  // Create Scene
-  this.scene = new THREE.Scene();
   /* Camera up must be the negative y so that the positive y is in the 
    * down direction and the positive x is in the right direction. If 
    * the camera up is set to the positive y, then positive x will be to 
    * the left. */
   this.camera.up = new THREE.Vector3(0,-1,0); 
   
+  // Create Scene
+  this.scene = new THREE.Scene();
+  
+  /** Set up generation code */
+  // Set level width and height
+  this.level_width = w;
+  this.level_height = h;
+  this.next_gen_loc = w;
+  // Get the number of blocks that fit on the screen vertically divided by 2
+  var vert_blocks = Math.round(h / 25) / 2;
+  this.gen_min_y = -vert_blocks * 25;
+  this.gen_max_y = vert_blocks * 25;
+  
+  // TODO Need to generate starting terrain (probably 3 screens worth) 
+  
   // TODO remove later
-  cube = new THREE.Mesh( new THREE.CubeGeometry( 25,25,25 ), new THREE.MeshNormalMaterial() );
+  this.cube = new THREE.Mesh( new THREE.CubeGeometry( 25,25,25 ), new THREE.MeshNormalMaterial() );
    //var material = new THREE.MeshLambertMaterial( { color: 0xFF0000 } );
-  cube.position.x = w/2;
-  cube.position.y = h/2;
-  this.scene.add(cube);
+  this.cube.position.x = w/2;
+  this.cube.position.y = h/2;
+  this.scene.add(this.cube);
 }
 
 /** Update the state of the level */
@@ -70,7 +81,7 @@ GameLevel.prototype.update = function(){
   // TODO 
   
   // TODO remove
-  cube.rotation.y += .01;
+  this.cube.rotation.y += .01;
 }
 
 /** Draw the level to the screen */
@@ -80,7 +91,7 @@ GameLevel.prototype.draw = function (renderer){
   this.camera.lookAt(new THREE.Vector3(this.camera_loc.x,this.camera_loc.y,0));
   
   // TODO 
-
+  
   // Draw scene 
   renderer.render( this.scene, this.camera );
 }
